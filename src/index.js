cb757036e952d92990c8337d1f9b13328b07e991
kiransahu-dev <sahukiran98.ks@gmail.com>
2023-04-11 20:35:25 +0530

    readerfile added

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ const fs = require("fs/promises");
 const fileName = "myfile.txt";
 const reader = async (fileName) => {
   // write code here
-	// dont change function name
-  
+  // dont change function name
+  const content = await fs.readFile(fileName);
+  return content.toString();
 };
 
 
-module.exports =  reader ;
+module.exports = reader;
